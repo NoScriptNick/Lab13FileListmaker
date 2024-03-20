c2e34df27f03f630ccd77e5788752b5d8e1eeedc
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2024-03-20 09:39:55 -0400

    sdfhsdfg

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,71 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
+
     public static ArrayList<String> list = new ArrayList<String>();
     public static Scanner scan = new Scanner(System.in);
-    public static boolean done = false;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        boolean loaded = false;
+        boolean edited = false;
+        boolean saved = false;
+
+        String fileName = null;
 
         String option = "";
-        list.addAll(Arrays.asList("Mr. Boolean", "Nick Beres", "Sub 5 minute mile", "LCL Victory", "Physics", "Books", "Math", "ReallyLongString", "Gamestop", "Barnes and Noble"));
+
+        boolean done = false;
+
         while (!done) {
 
-            option = InputHelper.getRegExString(scan, "Options: \nA – Add an item to the list \nD – Delete an item from the list \nP – Print the list \nQ – Quit the program", "[AaDdPpQq]");
+            option = InputHelper.getRegExString(scan, "Options: \nA – Add an item to the list \nD – Delete an item from the list \n" +
+                    "V – View the list \nO - Open \nS – Save \nC – Clear \nQ – Quit the program", "[AaDdVvQqOoSsCcQq]");
             if (option.equalsIgnoreCase("A")) {
                 add();
+                if (loaded) {
+                    edited = true;
+                    saved = false;
+                }
             } else if (option.equalsIgnoreCase("D")) {
                 delete();
-            } else if (option.equalsIgnoreCase("P")) {
-                print();
-            } else if (option.equalsIgnoreCase("Q")) {
+                if (loaded) {
+                    edited = true;
+                    saved = false;
+                }
+            } else if (option.equalsIgnoreCase("V")) {
+                view();
+            } else if (option.equalsIgnoreCase("O")) {
+                if (edited) {
+                    if (loaded) {
+                        IOHelper.writeFile(list, fileName);
+                    } else {
+                        if (InputHelper.getYNConfirm(scan, "Do you want to save the current list before opening the new file?")) {
+                        }
+                    }
+                }
+                list.clear();
+                fileName = IOHelper.openFile(list);
+                loaded = true;
+                edited = false;
+                saved = false;
+            } else if (option.equalsIgnoreCase("S")) {
+                if (loaded) {
+                    IOHelper.writeFile(list, fileName);
+                } else {
+                    IOHelper.writeFile(list, InputHelper.getNonZeroLenString(scan, "Name your file"));
+                }
+                loaded = false;
+                edited = false;
+                saved = true;
+            } else if (option.equalsIgnoreCase("C")) {
+                list.clear();
+                loaded = true;
+                edited = false;
+                saved = false;
+            }
+            else if (option.equalsIgnoreCase("Q")) {
                 quit();
             }
         }
@@ -34,14 +81,14 @@ public class Main {
         list.remove(index);
     }
 
-    public static void print(){
-        System.out.println(list);
+    public static void view(){
         for (int i = 0; i < list.size(); i++) {
             System.out.println(i + " - " + list.get(i));
         }
     }
 
     public static void quit(){
+        boolean done = false;
         done = InputHelper.getYNConfirm(scan, "Are you sure you want to quit? [Y/N]");
     }
 }
